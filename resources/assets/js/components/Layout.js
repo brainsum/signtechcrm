0a51d9586d0ec37e592052e51bbf98822668712e
Décsi Dániel <ddecsi@brainsum.com>
2016-11-02 11:59:46 +0100

    Footer

--- resources/assets/js/components/Layout.js
+++ resources/assets/js/components/Layout.js
@@ -4,15 +4,19 @@ import { BrowserRouter, Match } from 'react-router';
 import HomePage from 'app/components/pages/HomePage';
 import LoginPage from 'app/components/pages/LoginPage';
 import RegistrationPage from 'app/components/pages/RegistrationPage';
+import Footer from 'app/components/partials/Footer';
 
 export default () => (
-    <BrowserRouter>
-        <div>
-            <Header />
+    <div>
+        <BrowserRouter>
+            <div>
+                <Header />
 
-            <Match exactly pattern="/" component={HomePage} />
-            <Match pattern="/login" component={LoginPage} />
-            <Match pattern="/registration" component={RegistrationPage} />
-        </div>
-    </BrowserRouter>
+                <Match exactly pattern="/" component={HomePage} />
+                <Match pattern="/login" component={LoginPage} />
+                <Match pattern="/registration" component={RegistrationPage} />
+            </div>
+        </BrowserRouter>
+        <Footer />
+    </div>
 );
---
+++ resources/assets/js/components/partials/Footer.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default () => (
+    <footer className="footer">
+        <img src="/img/paperless-forms.png" alt="Signtech logo copyright paperless forms" />
+        Copyright © 2012 E-beeze Ltd (04091591). All rights reserved. Powered by: <a href="https://e-beeze.co.uk/" target="_blank">E-beeze</a>
+    </footer>
+);
