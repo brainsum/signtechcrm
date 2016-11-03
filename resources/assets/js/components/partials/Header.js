b65193cfe24841b7584ea84d82ac86e293f4a8b6
Décsi Dániel <ddecsi@brainsum.com>
2016-11-03 14:22:52 +0100

    Removed SignTech logo

--- resources/assets/js/components/partials/Header.js
+++ resources/assets/js/components/partials/Header.js
@@ -4,7 +4,9 @@ import Navigation from 'app/components/partials/Navigation';
 export default () => (
     <header className="header container">
         <div className="header__inner">
-            <div className="header__logo">SignTech & SQR logos</div>
+            <div className="header__logos">
+                <img src="/img/sqr.jpg" alt="SQR Security Solutions" />
+            </div>
             <div className="header__nav">
                 <Navigation />
             </div>
