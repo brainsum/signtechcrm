2a65019fc93ed3f4ab3a901d5959858468bfbf3e
danieldecsi <d@niel.hu>
2016-11-16 12:00:27 +0100

    Linked SQR logo to home page

--- resources/assets/js/components/partials/Header.js
+++ resources/assets/js/components/partials/Header.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import Navigation from 'app/components/partials/Navigation';
 
 export default () => (
     <header className="header container">
         <div className="header__inner">
             <div className="header__logos">
-                <img src="/img/sqr.jpg" alt="SQR Security Solutions" />
+                <Link to="/">
+                    <img src="/img/sqr.jpg" alt="SQR Security Solutions" />
+                </Link>
             </div>
             <div className="header__nav">
                 <Navigation />
