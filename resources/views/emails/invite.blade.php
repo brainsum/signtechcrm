bc6f1ad08829ea89409d328a18c38bf410a45618
danieldecsi <d@niel.hu>
2016-11-23 16:26:19 +0100

    Added a line break in the invitation e-mail for easier password copying

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -1,4 +1,4 @@
 <p>Dear <?=$name?>,</p>
 <p>A new user account has been created for you on <?=$siteName?>.</p>
-<p>Your password is: <?=$password?>. Please change it in My account after first login.</p>
+<p>Your password is: <?=$password?><br>Please change it in My account after first login.</p>
 <p><a href="<?=$baseUrl?>"><?=$baseUrl?></a></p>
